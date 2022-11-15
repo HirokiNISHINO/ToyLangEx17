20b453967de8f8239c6488cdd74e509601375037
Hiroki NISHINO <nishino.hiroki@kochi-tech.ac.jp>
2022-11-15 17:13:34 +0900

    Fixed a wrong sysexit syscall no. for LINUX.

--- src/kut/compiler/compiler/CodeGenerator.java
+++ src/kut/compiler/compiler/CodeGenerator.java
@@ -25,7 +25,7 @@ public class CodeGenerator
 {
 	protected Platform		platform;
 	protected String 		filename;
-	protected AstNode			program	;
+	protected AstNode		program	;
 	protected PrintWriter 	writer	;
 	
 	protected SymbolTable	symbolTable;
@@ -58,7 +58,7 @@ public class CodeGenerator
 	 */
 	public String getExitSysCallNum()
 	{
-		return (this.platform == Platform.MAC ? "0x2000001" : "0x01");
+		return (this.platform == Platform.MAC ? "0x2000001" : "60");
 	}
 	
 	/**
